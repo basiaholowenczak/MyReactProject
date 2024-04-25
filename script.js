9e0dcacc683b0f6527e980eb081a6616d8462af3
apanga01 <pallyssam@gmail.com>
2024-04-25 12:25:40 -0400

    updated script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const fetchRecipes = async (query) => {
     response.meals.forEach(meal => {
         const recipeDiv = document.createElement('div');
         recipeDiv.classList.add('recipe');
-        recipeDiv.innerHTML = '<img src = "${meal.strMealThumb}"> <h3>${meal.strMeal}</h3> <p><span>${meal.Area}</span> Dish</p> <p> Belongs to<span>${meal.Category}</span> Category</p>'
+        recipeDiv.innerHTML = `<img src="${meal.strMealThumb}"> <h3>${meal.strMeal}</h3> <p><span>${meal.Area}</span> Dish</p> <p>Belongs to<span>${meal.Category}</span> Category</p>`;
 
         const button = document.createElement('button');
         button.textContent = "View Recipe";
@@ -43,7 +43,7 @@ for(let i = 1; i<20; i++){
     const ingredient = meal[`strIngredient${i}`];
     if (ingredient) {
         const measure = meal[`strMeasure${i}`];
-        ingredientsList += '<li>${measure} ${ingredient}</li>'
+        ingredientsList += `<li>${measure} ${ingredient}</li>`;
     }
     else{
         break;
