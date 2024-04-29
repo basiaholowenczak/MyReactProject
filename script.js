bb8ac6b267c7d58491ca5090b0cc908c8997955d
Basia Holowenczak <bholowen@villanova.edu>
2024-04-29 09:38:37 -0400

    modified styles.css to not override styling details of child element

--- script.js
+++ script.js
@@ -15,14 +15,18 @@ const fetchRecipes = async (query) => {
         response.meals.forEach(meal => {
             const recipeDiv = document.createElement('div');
             recipeDiv.classList.add('recipe');
-            recipeDiv.innerHTML = `<img src="${meal.strMealThumb}"> <h3>${meal.strMeal}</h3> <p><span>${meal.strArea}</span> Dish</p> <p>Belongs to<span>${meal.strCategory}</span> Category</p>`;
+            recipeDiv.innerHTML = 
+                `<img src="${meal.strMealThumb}"> 
+                <h3>${meal.strMeal}</h3> 
+                <p><span>${meal.strArea}</span> Dish</p> 
+                <p>Belongs to <span>${meal.strCategory}</span> Category</p>`;
 
             const button = document.createElement('button');
             button.textContent = "View Recipe";
             recipeDiv.appendChild(button);
 
             // Adding EventListener to recipe button 
-            button.addEventListener('click', () => {
+            button.addEventListener('click', ()=> {
                 openRecipePopup(meal);
             });
 
@@ -36,12 +40,13 @@ const fetchRecipes = async (query) => {
 // Function to fetch ingredients and measurements
 const fetchIngredients = (meal) => {
     let ingredientsList = "";
-    for (let i = 1; i < 20; i++) {
+    for (let i = 1; i <= 20; i++) {
         const ingredient = meal[`strIngredient${i}`];
         if (ingredient) {
             const measure = meal[`strMeasure${i}`];
             ingredientsList += `<li>${measure} ${ingredient}</li>`;
-        } else {
+        } 
+        else {
             break;
         }
     }
@@ -49,9 +54,16 @@ const fetchIngredients = (meal) => {
 }
 
 const openRecipePopup = (meal) => {
-    recipeDetailsContent.innerHTML = `<h2 class="recipeName">${meal.strMeal}</h2> <h3>Ingredients:</h3> <ul class="ingredientsList">${fetchIngredients(meal)}</ul> <div class="recipeInstructions"><h3>Instructions:</h3> <p class="recipeInstructions">${meal.strInstructions}</p> </div>`;
-
-    recipeDetailsContent.parentElement.style.display = "block";
+    recipeDetailsContent.innerHTML = 
+        `<h2 class="recipeName">${meal.strMeal}</h2> 
+        <h3>Ingredients:</h3> 
+        <ul class = "ingedientList">${fetchIngredients(meal)}</ul>
+        <div class = recipeInstructions>
+            <h3>Ingredients:</h3> 
+            <p>${meal.strInstructions}</p>
+        </div>
+        `
+        ;
 }
 
 recipeCloseBtn.addEventListener('click', () => {
